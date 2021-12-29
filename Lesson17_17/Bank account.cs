75547da353c272c4face8415036a8a68e2d5ef4b
anvarovagulnaz <anvarovagulnaz@gmail.com>
2021-12-29 19:35:59 +0500

    Добавьте файлы проекта.

---
+++ Lesson17_17/Bank account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson17_17
+{
+    class Bank_account<T>
+    {
+        public T Number { get; set; }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+
+        public Bank_account (T number, int balance, string name)
+        {
+            Number = number;
+            Balance=balance;
+            Name = name;
+
+        }
+        public string GetInfo()
+        {
+            return $"{Number} {Balance} {Name}";
+            
+            
+        }
+    }
+}
---
+++ Lesson17_17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson17_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовой номер счета");
+            int numberInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите баланс");
+            int balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            string name = Console.ReadLine();
+
+            Bank_account<int> Bank_account1 = new Bank_account<int>(numberInt, balance, name);
+            Console.WriteLine(Bank_account1.GetInfo());
+
+            Console.WriteLine("Введите строковый номер счета");
+            string numberString = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            name = Console.ReadLine();
+
+            Bank_account<string> Bank_account2 = new Bank_account<string>(numberString, balance, name);
+            Console.WriteLine(Bank_account2.GetInfo());
+            Console.ReadKey();
+        }
+    }
+}
